9f525d75f26da1e5d07b7a906d8181d87833dc76
Niko <niko-sss@users.noreply.github.com>
2026-02-04 10:49:07 +0200

    1.1

--- week3/test/conversion.expect.test.js
+++ week3/test/conversion.expect.test.js
@@ -2,6 +2,8 @@
 // 04.02.2026 - DD/MM/YYYY
 // Author: Niko Sukari
 
+
+// Requires files
 const { expect } = require("chai");
 const { hexToRgbConversion } = require("../src/conversion");
 
@@ -10,10 +12,14 @@ describe("conversion.js", () => {
 
   describe("hexToRgbConversion", () => {
 
-    it("should convert hex to rgb", () => {
+    // Test conversion function with pre-determined hexadecimal.
+    // Expects to return proper RGB-value.
+    it("should convert pre-determined hexadecimal to corresponding RGB-value properly", () => {
       expect(hexToRgbConversion("f1339b")).to.equal("RGB: 241, 51, 155");
     });
 
+    // Test conversion function with a non-string parameter.
+    // Expects to throw an error.
    it("should throw an error if input is not a string", () => {
        expect(() => hexToRgbConversion(33)).to.throw("Hexadecimal should be String.")
        expect(() => hexToRgbConversion(null)).to.throw("Hexadecimal should be String.")
@@ -21,14 +27,20 @@ describe("conversion.js", () => {
        expect(() => hexToRgbConversion(true)).to.throw("Hexadecimal should be String.")
     });
     
+    // Test conversion function with too short parameter.
+    // Expects to throw an error.
     it("should throw an error if input is too short", () => {
         expect(() => hexToRgbConversion("f123")).to.throw("Hexadecimal should be 6 digits long.");
     });
     
+    // Test conversion function with too long parameter.
+    // Expects to throw an error.
     it("should throw an error if input is too long", () => {
         expect(() => hexToRgbConversion("3344556677")).to.throw("Hexadecimal should be 6 digits long.");
     });
     
+    // Test conversion function with values not ranging from hexadecimal range of 0-9 and a-f.
+    // Expects to throw an error.
     it("should throw an error if input is not a proper hexadecimal from 0-9 and a-f", () => {
         expect(() => hexToRgbConversion("f1339w")).to.throw("Not a hexadecimal");
     });
