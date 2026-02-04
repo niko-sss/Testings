27b3fd9057d93f5e88a777ca462778699ec0064b
Niko <niko-sss@users.noreply.github.com>
2026-02-04 10:38:00 +0200

    1.0

--- week3/test/server.expect.test.js
+++ week3/test/server.expect.test.js
@@ -24,27 +24,29 @@ describe("RGB conversion API", () => {
 
 
 	// Test the /conversion route with query parameters.
-	// Expects HTTP status 200.
-
+	// Expects HTTP status code 200.
 	it("should return status code 200 for /convert route", async () => {
 		const res = await request(app).get("/convert?hex=f1f1f1");
 		expect(res.status).to.equal(200);
 	});
 
-	/**
-	 * Test the /add route for correct sum calculation.
-	 * Expects the sum of two numbers in response text.
-	 */
+
+	// Test the /convert route for correct hex to rgb conversion.
+	// Expects proper conversion for pre-determined hexadecimal.
 	it("should return proper RGB value for corresponding hexadecimal values", async () => {
 		const res = await request(app).get("/convert?hex=f1339b");
 		expect(res.text).to.equal("RGB: 241, 51, 155");
 	});
 
+	// Test an invalid route.
+	// Expects to return status code 404 as route should not exist.
 	it("should return response code 404 for invalid route", async () => {
 	const res = await request(app).get("/notaroute");
 	expect(res.statusCode).to.equal(404);
 	});
 
+	// Test /convert route with invalid query parameter
+	// Expects to return status code 500.
 	it("should return response code 500 for invalid query parameter", async () => {
 	const res = await request(app).get("/convert?hex=f132239b");
 	expect(res.statusCode).to.equal(500);
